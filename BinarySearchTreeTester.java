4929302ef310f3f3428c1d8025fd232e8ab1f4f4
Ivan M <ivanmedina@csus.edu>
2019-10-28 18:33:43 -0700

    Formatted text for test cases in BinarySearchTreeTester.java

--- BinarySearchTreeTester.java
+++ BinarySearchTreeTester.java
@@ -13,9 +13,6 @@ public class BinarySearchTreeTester{
       System.out.println();
       tree.remove(10);
       
-      System.out.println("\n\n");
-      tree.printTree();
-      
       //Check contains method functionality
       for(int i = 0; i < 15; i++) {
          System.out.println("Tree contains " +i +": " +tree.contains(i));
@@ -23,31 +20,52 @@ public class BinarySearchTreeTester{
       
       //Check remove method functionality with different cases
       System.out.println("\nRemove a node with two children (8):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(8);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
-      System.out.println("\n\nRemove a node with one child (left child):");
+      System.out.println("\n\nRemove a node with one child (left child) (9):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(9);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
       System.out.println("\n\nRemove a leaf node (7):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(7);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
       System.out.println("\n\nRemove a leaf node(0):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(0);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
-      System.out.println("\n\nRemove a node with one child (right child)");
+      System.out.println("\n\nRemove a node with one child (right child) (1):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(1);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
-      System.out.println("\n\nRemove the root:");
+      System.out.println("\n\nRemoving the root (6):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(6);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
-      System.out.println("\n\nRemoving a node that does not exist (10): ");
+      System.out.println("\n\nRemoving a node that does not exist (10):");
+      System.out.println("Before:");
+      tree.printTree();
       tree.remove(10);
+      System.out.println("\nAfter: ");
       tree.printTree();
       
       //Check clear method fucntionality
